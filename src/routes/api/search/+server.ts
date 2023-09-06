d8b9f6afb220aa3385ac7011de30ecda6f2607ff
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-09-06 14:41:00 +0600

    ..

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -2,23 +2,39 @@ import { supabase } from "$lib/server/supabaseclient.server";
 import type { RequestEvent } from "./$types";
 let req=
 {
-    recommendation:"",
+    recommendation:false,
     search_term:"",
 }
 export async function POST({request, cookies}: RequestEvent): Promise<Response>
 { 
     req = await request.json();
+    let ret_text;
     //console.log(req);
+    if(req.recommendation == true)
+    {
+
     let term=req.search_term
     let { data:result, error } = await supabase
     .rpc('preempt_search_service2', {
     term
     })
-    if (error) console.error(error)
+    if (error)
+    {
+        ret_text={
+            errorcode:-1
+          }
+    } 
+    else
+    {
+        ret_text=result;
+    }
+
+    }
+    
 
     //console.log(result);
 
-    return new Response(JSON.stringify(result), {
+    return new Response(JSON.stringify(ret_text), {
 		headers: {
 			'Content-Type': 'application/json'
 		}
