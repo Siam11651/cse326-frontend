6f12f90abb1a91f066fd25c0de41aabca1baa46c
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-09-06 19:25:22 +0600

    ..

---
+++ src/routes/api/provider_list/+server.ts
@@ -0,0 +1,30 @@
+import { supabase } from "$lib/server/supabaseclient.server";
+import type { RequestEvent } from "./$types";
+
+export async function POST({request, cookies}: RequestEvent): Promise<Response>
+{
+    let ret_text;
+
+let given_serviceid=1;
+let { data:result, error } = await supabase
+.rpc('get_matching_providers_service', {
+  given_serviceid
+})
+
+if (error) 
+{
+    ret_text={
+        errorcode:-1
+      }
+}
+else 
+{
+    ret_text=result;
+}
+
+    return new Response(JSON.stringify(ret_text), {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
