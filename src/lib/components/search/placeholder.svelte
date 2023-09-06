f159ee43d7fcbd5fcf539c8658155f9e44531eb5
Siam11651 <siam11651@outlook.com>
2023-09-06 14:32:06 +0600

    kaj kori

---
+++ src/lib/components/search/placeholder.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    export let index: number;
+</script>
+
+<!-- svelte-ignore a11y-invalid-attribute -->
+<a class="list-group-item list-group-item-action placeholder-wave" href="#">
+    <h6><span class="placeholder col-3" /></h6>
+    <p class="text-body-secondary text-truncate">
+        <span class="placeholder col-6" />
+        <span class="placeholder col-3" />
+    </p>
+    <div class="d-flex flex-row-reverse">
+        <div class="info-tag input-group mb-3 ms-2">
+            <span class="input-group-text" id="address-{index}" title="Location of Service">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                </svg>
+            </span>
+            <input type="text" class="form-control" aria-label="address" aria-describedby="address-{index}" disabled>
+        </div>
+        <div class="info-tag input-group mb-3 ms-2">
+            <span class="input-group-text" id="rate-{index}" title="Rate of Service">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                </svg>
+            </span>
+            <input type="number" class="form-control" aria-label="rate" aria-describedby="rate-{index}" disabled>
+        </div>
+        <div class="info-tag input-group mb-3">
+            <span class="input-group-text" id="price-{index}" title="Price of Service">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags-fill" viewBox="0 0 16 16">
+                    <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                    <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z"/>
+                </svg>
+            </span>
+            <input type="text" class="form-control" aria-label="price" aria-describedby="price-{index}" disabled>
+        </div>
+    </div>
+</a>
+
+<style lang="scss">
+    .info-tag
+    {
+        max-width: 10em;
+    }
+</style>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,92 @@
 <script lang="ts">
     import Navbar from "$lib/components/navbar.svelte";
+    import { onMount } from "svelte";
     import { fade, scale } from "svelte/transition";
+    import type { SearchResult } from "./search-result";
+    import Result from "$lib/components/search/result.svelte";
+    import Placeholder from "$lib/components/search/placeholder.svelte";
+    import { SorterType, type Sorter, SorterImplementation } from "./sorter";
+
+    let searchResults: SearchResult[] = new Array<SearchResult>(10);
+
+    for(let i: number = 0; i < 10; ++i)
+    {
+        searchResults[i] =
+        {
+            id: i,
+            title: "Service " + (i + 1),
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium ultrices nibh in scelerisque. Proin facilisis dolor a est eleifend, congue egestas lorem convallis. Nam porta aliquam mi. Nullam ligula tellus, aliquam vitae magna congue, dignissim feugiat sapien. Sed vulputate commodo eros tempus mollis. Ut vestibulum blandit ornare. Sed id semper massa. Ut augue libero, efficitur a orci eget, commodo volutpat augue. Proin in neque turpis. Nullam pretium iaculis nisl, non pellentesque purus feugiat a. Vivamus malesuada augue vel nisl faucibus, eu bibendum purus posuere. Suspendisse tristique ut libero volutpat auctor.",
+            price: 69
+        };
+    }
+
+    let sortMethod: string = SorterImplementation.GetSorterName(SorterType.NAME_ASC);
+    let sorters: Sorter[] =
+    [
+        {
+            name: SorterImplementation.GetSorterName(SorterType.NAME_ASC),
+            handler: (): void =>
+            {
+                sortMethod = SorterImplementation.GetSorterName(SorterType.NAME_ASC);
+                searchResults = SorterImplementation.ApplyFilter(searchResults, SorterType.NAME_ASC)
+            }
+        },
+        {
+            name: SorterImplementation.GetSorterName(SorterType.NAME_DESC),
+            handler: (): void =>
+            {
+                sortMethod = SorterImplementation.GetSorterName(SorterType.NAME_DESC);
+                searchResults = SorterImplementation.ApplyFilter(searchResults, SorterType.NAME_DESC)
+            }
+        },
+        {
+            name: SorterImplementation.GetSorterName(SorterType.PRICE_ASC),
+            handler: (): void =>
+            {
+                sortMethod = SorterImplementation.GetSorterName(SorterType.PRICE_ASC);
+                searchResults = SorterImplementation.ApplyFilter(searchResults, SorterType.PRICE_ASC)
+            }
+        },
+        {
+            name: SorterImplementation.GetSorterName(SorterType.PRICE_DESC),
+            handler: (): void =>
+            {
+                sortMethod = SorterImplementation.GetSorterName(SorterType.PRICE_DESC);
+                searchResults = SorterImplementation.ApplyFilter(searchResults, SorterType.PRICE_DESC)
+            }
+        }
+    ]
+
+    onMount((): void =>
+    {
+        let urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+        let encodedTerm: string | null = urlParams.get("q");
+
+        if(encodedTerm)
+        {
+            let searchTerm: string = decodeURI(encodedTerm);
+            let requestObject = 
+            {
+                recommendation: true,
+                search_term: searchTerm
+            };
+
+            let requestBodyString = JSON.stringify(requestObject);
+
+            fetch("/api/search",
+            {
+                method: "POST",
+                headers:
+                {
+                    "Content-Type": "application/json"
+                },
+                body: requestBodyString
+            }).then(async (response: Response): Promise<void> =>
+            {
+                
+            });
+        }
+    });
 </script>
 
 <div class="search-root">
@@ -42,54 +128,31 @@
                     <div class="fs-5">Top Results</div>
                     <div class="dropdown">
                         <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Sort By: {"Name"}
+                            Sort By: {sortMethod}
                         </button>
                         <ul class="dropdown-menu">
-                          <li><button class="dropdown-item">Name</button></li>
-                          <li><button class="dropdown-item">Created</button></li>
-                          <li><button class="dropdown-item">Popularity</button></li>
+                            {#each sorters as {name, handler}}
+                                <li>
+                                    <button class="dropdown-item" on:click={handler}>
+                                        {name}
+                                    </button>
+                                </li>
+                            {/each}
                         </ul>
                     </div>
                 </div>
                 <div class="all-results-padder shadow-lg p-3 me-5 rounded" in:scale={{duration: 500, start: 0.95}}>
                     <div class="all-results-container">
                         <ul class="list-group list-group-flush">
-                            {#each [...Array(10).keys()] as i}
-                                <!-- svelte-ignore a11y-invalid-attribute -->
-                                <a class="list-group-item list-group-item-action" href="#">
-                                    <h6>Service {i + 1}</h6>
-                                    <p class="text-body-secondary text-truncate">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget libero posuere, aliquet elit sit amet, lacinia urna. Quisque id tellus sed libero dictum efficitur. Etiam quis euismod leo. Vestibulum nec velit vel nunc sodales consectetur. Fusce facilisis ut elit ac cursus. Sed tempor fermentum tortor, fermentum facilisis nisi ullamcorper ac. Maecenas sapien diam, fermentum sit amet dictum quis, sollicitudin in quam. Donec rutrum ipsum sit amet mi malesuada, vitae egestas quam convallis. Donec eu mattis libero. Pellentesque in viverra sapien. Etiam sagittis elit eget tristique ornare. Sed in urna in neque efficitur laoreet. Etiam non est ac erat efficitur placerat.
-                                    </p>
-                                    <div class="d-flex flex-row-reverse">
-                                        <div class="info-tag input-group mb-3 ms-2">
-                                            <span class="input-group-text" id="address-{i}" title="Location of Service">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                                                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                                                </svg>
-                                            </span>
-                                            <input type="text" class="form-control" aria-label="address" aria-describedby="address-{i}" value={"Kauyerchor"} disabled>
-                                        </div>
-                                        <div class="info-tag input-group mb-3 ms-2">
-                                            <span class="input-group-text" id="rate-{i}" title="Rate of Service">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                                </svg>
-                                            </span>
-                                            <input type="number" class="form-control" aria-label="rate" aria-describedby="rate-{i}" value={4.2} disabled>
-                                        </div>
-                                        <div class="info-tag input-group mb-3">
-                                            <span class="input-group-text" id="price-{i}" title="Price of Service">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags-fill" viewBox="0 0 16 16">
-                                                    <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-                                                    <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z"/>
-                                                </svg>
-                                            </span>
-                                            <input type="text" class="form-control" aria-label="price" aria-describedby="price-{i}" value="৳{69}" disabled>
-                                        </div>
-                                    </div>
-                                </a>
-                            {/each}
+                            {#if searchResults.length == 0}
+                                {#each [...Array(4).keys()] as i}
+                                    <Placeholder index={i} />
+                                {/each}
+                            {:else}
+                                {#each searchResults as {id, title, description, price}}
+                                    <Result id={id} title={title} description={description} price={price} />
+                                {/each}
+                            {/if}
                         </ul>
                     </div>
                 </div>
@@ -141,10 +204,5 @@
     {
         max-height: 100%;
         overflow-y: auto;
-    }
-
-    .info-tag
-    {
-        max-width: 10em;
     }
 </style>
