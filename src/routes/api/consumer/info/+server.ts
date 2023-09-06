71d10837d459bb1a4c062ab0d60dfcd1d4f1fe4e
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-09-06 19:30:48 +0600

    ..

---
+++ src/routes/api/consumer/info/+server.ts
@@ -0,0 +1,30 @@
+import { supabase } from "$lib/server/supabaseclient.server";
+import type { RequestEvent } from "./$types";
+
+export async function POST({request, cookies}: RequestEvent): Promise<Response>
+{
+    let ret_text;
+
+let given_cname=1;
+
+let { data:result, error } = await supabase
+  .rpc('get_consumer_details', {
+    given_cname
+  })
+if (error) 
+{
+    ret_text={
+        errorcode:-1
+      }
+}
+else 
+{
+    ret_text=result;
+}
+
+    return new Response(JSON.stringify(ret_text), {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
