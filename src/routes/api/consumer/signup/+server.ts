1b029531e4135ad4a3dffd97679da2c315897561
Siam11651 <siam11651@outlook.com>
2023-09-06 06:38:46 +0600

    kaj kori

--- src/routes/api/consumer/signup/+server.ts
+++ src/routes/api/consumer/signup/+server.ts
@@ -19,71 +19,69 @@ export async function POST({request, cookies}: RequestEvent): Promise<Response>
     let given_mail=user.email;
     let { data:result, error } = await supabase
     .rpc('is_consumer_in_table', {
-        given_cname,
-        given_mail
+      given_cname,
+      given_mail
     })
   let ret_text;
   if (error) 
   {
     ret_text={
-        errorcode:-1
+      errorcode:-1
     }
   }
   else 
   {
     if(result == true)
     {
-        ret_text={
-            errorcode:-2
-        }
+      ret_text={
+          errorcode:-2
+      }
 
     }
     else
     {
         
-  let given_billingaddress=''; 
-  let given_cname=user.name; 
-  let given_contactnumber=user.contact; 
-  let given_imagefile=null;
-  if(user.pfp)
-  {
-    given_imagefile=user.pfp;
-  } 
-  let given_mail=user.email; 
-  let given_security_key=user.password_hash;
+      let given_billingaddress=''; 
+      let given_cname=user.name; 
+      let given_contactnumber=user.contact; 
+      let given_imagefile=null;
+      if(user.pfp)
+      {
+        given_imagefile=user.pfp;
+      } 
+      let given_mail=user.email; 
+      let given_security_key=user.password_hash;
         
-let { data:result, error } = await supabase
-.rpc('insert_consumer', {
-  given_billingaddress, 
-  given_cname, 
-  given_contactnumber, 
-  given_imagefile, 
-  given_mail, 
-  given_security_key
-})
+      let { data:result, error } = await supabase
+      .rpc('insert_consumer', {
+        given_billingaddress, 
+        given_cname, 
+        given_contactnumber, 
+        given_imagefile, 
+        given_mail, 
+        given_security_key
+      });
 
-if (error) 
-{
-    ret_text={
-        errorcode:-3
+      if (error) 
+      {
+        ret_text={
+          errorcode:-3
+        }
+      }
+      else 
+      {
+        let ret_user=
+        {
+          id:result,
+          is_consumer:true
+        }
+        const token = jwt.sign(ret_user, import.meta.env.VITE_JWT_KEY, { expiresIn: `${15 * 86400 * 1000}` });
+        ret_text={
+          errorcode:0,
+          jwt_token:token
+        }
+      }
     }
-}
-else 
-{
-    let ret_user=
-    {
-      id:result,
-      is_consumer:true
-    }
-    const token = jwt.sign(ret_user, import.meta.env.VITE_JWT_KEY, { expiresIn: `${15 * 86400 * 1000}` });
-    ret_text={
-      errorcode:0,
-      jwt_token:token
-    }
-}
-
-    }
-
   }
 
     return new Response(JSON.stringify(ret_text), {
