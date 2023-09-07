1ddf52e4e3b05c72f8dc21466ec816abe4f210db
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-09-07 23:16:57 +0600

    sort api

---
+++ src/routes/api/sort/cost_sort/+server.ts
@@ -0,0 +1,35 @@
+import { supabase } from "$lib/server/supabaseclient.server";
+import type { RequestEvent } from "./$types";
+
+export async function POST({request, cookies}: RequestEvent): Promise<Response>
+{ 
+    //req = await request.json();
+    let ret_text;
+    let given_serviceid=1;
+    let { data:result, error } = await supabase
+  .rpc('get_matching_providers_service_cost', {
+    given_serviceid
+  })
+    if (error)
+    {
+        ret_text={
+            errorcode:-1
+          }
+          console.log(error);
+    } 
+    else
+    {
+        ret_text=result;
+    }
+
+
+        
+
+    //console.log(result);
+
+    return new Response(JSON.stringify(ret_text), {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
