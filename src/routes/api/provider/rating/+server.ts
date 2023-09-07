b3292ad87c76a975e360d251ab2aa750fca249d6
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-09-07 18:32:17 +0600

    cookie added in provider

--- src/routes/api/provider/rating/+server.ts
+++ src/routes/api/provider/rating/+server.ts
@@ -1,26 +1,42 @@
 import { supabase } from "$lib/server/supabaseclient.server";
 import type { RequestEvent } from "./$types";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
 export async function POST({request, cookies}: RequestEvent): Promise<Response>
 {
     let ret_text;
+    let token: string | undefined = cookies.get("pjwt");
+    
+    if(token)
+    {
+      let decodedToken = jwt.verify(token, import.meta.env.VITE_JWT_KEY);
+      let _pid=decodedToken.id;
 
-let _pid=1;
+            let { data:result, error } = await supabase
+              .rpc('get_rating_of_provider', {
+                _pid
+              })
+            if (error) 
+            {
+                ret_text={
+                    errorcode:-1
+                  }
+            }
+            else 
+            {
+                ret_text=result;
+            }
 
-let { data:result, error } = await supabase
-  .rpc('get_rating_of_provider', {
-    _pid
-  })
-if (error) 
-{
-    ret_text={
-        errorcode:-1
-      }
-}
-else 
-{
-    ret_text=result;
-}
+    }
+    else
+    {
+      ret_text={
+        errorcode:-2
+      } 
+      console.log("No Token");
+    }
+
+
 
     return new Response(JSON.stringify(ret_text), {
 		headers: {
