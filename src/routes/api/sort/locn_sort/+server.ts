6e7beee9f1a011dd3d8d55e7b5178afa8a0c5a08
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2023-09-10 23:46:10 +0600

    geodist

--- src/routes/api/sort/locn_sort/+server.ts
+++ src/routes/api/sort/locn_sort/+server.ts
@@ -1,13 +1,13 @@
 import { supabase } from "$lib/server/supabaseclient.server";
 import type { RequestEvent } from "./$types";
-import {getDistance} from 'geolib';
+import { getDistance } from "geolib";
 
 export async function POST({
   request,
   cookies,
 }: RequestEvent): Promise<Response> {
   let req = await request.json();
-  console.log("locn: " , req);
+  console.log("locn: ", req);
   let ret_text;
   let given_serviceid = req.service_id;
   let { data: result, error } = await supabase.rpc(
@@ -23,18 +23,18 @@ export async function POST({
     console.log(error);
   } else {
     let consumer_coords = {
-        lat: req.latitude,
-        lon: req.longitude
-    }
-    
+      lat: req.latitude,
+      lon: req.longitude,
+    };
+
     for (let i = 0; i < result.length; i++) {
-        let provider_coords = {
-            lat: result[i].coordinates.split(',')[0],
-            lon: result[i].coordinates.split(',')[1]
-        }
-        result[i].distance = getDistance(consumer_coords, provider_coords)
+      let provider_coords = {
+        lat: result[i].coordinates.split(",")[0],
+        lon: result[i].coordinates.split(",")[1],
+      };
+      result[i].distance = getDistance(consumer_coords, provider_coords);
     }
-    
+
     ret_text = result;
   }
 
