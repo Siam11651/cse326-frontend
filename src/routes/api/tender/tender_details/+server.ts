bf88a611d9004bdf8282af7263bd6b9bedcb306b
Sadif-Ahmed <1905058@ugrad.cse.buet.ac.bd>
2023-09-11 22:25:05 +0600

    tender_details

---
+++ src/routes/api/tender/tender_details/+server.ts
@@ -0,0 +1,40 @@
+import { supabase } from "$lib/server/supabaseclient.server";
+import type { RequestEvent } from "./$types";
+
+export async function POST({
+  request,
+  cookies,
+}: RequestEvent): Promise<Response> {
+  let ret_text;
+
+  let requestBody = await request.json();
+  
+  let serviceid= requestBody.serviceid;
+
+
+  let given_tenderid = requestBody.tender_id;
+
+  let { data:result, error } = await supabase
+  .rpc('get_single_tender_details', {
+    given_tenderid
+  })
+
+if (error)
+{
+    ret_text={
+        errocode:-1,
+    };
+
+} 
+else 
+{
+    ret_text = result;
+}
+
+
+  return new Response(JSON.stringify(ret_text), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
